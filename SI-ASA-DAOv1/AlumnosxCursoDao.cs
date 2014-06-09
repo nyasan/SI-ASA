03785ccc9d1f2eaeb9d51f8bf7238fc449b639b3
nyasan <cesar.alcaide08@gmail.com>
2014-06-08 21:52:25 -0300

    Subo nueva transaccion.

---
+++ SI-ASA-DAOv1/AlumnosxCursoDao.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SI_ASA_ENTIDADESv1;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SI_ASA_DAOv1
+{
+    public class AlumnosxCursoDao
+    {
+        private String conexion = "";
+
+        public static void add(List<Alumno> listAlumnos, int id_curso, DateTime fechaInscripcion)
+        {
+            int i = -1;
+            String sql = "INSERT INTO alumnos_x_curso (legajo, id_curso, fecha_inscripcion)";
+            sql += "VALUES (@legajo, @id_curso, @fecha_inscripcion)";
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=ALEBELTRAMEN\\ALEJANDRA;Initial Catalog=ASA;Integrated Security=True";
+            SqlTransaction sqltran = cn.BeginTransaction();
+
+            try
+            {
+                cn.Open();
+                
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                cmd.Transaction = sqltran;
+             
+                while(i<listAlumnos.Count)
+                {
+                    cmd.Parameters.AddWithValue("@legajo", listAlumnos.ElementAt(i).legajo);
+                    cmd.Parameters.AddWithValue("@id_curso", id_curso);
+                    cmd.Parameters.AddWithValue("@fecha_inscripcion", fechaInscripcion);
+                    cmd.ExecuteNonQuery();
+                    i++;
+                }
+
+                sqltran.Commit();
+                cn.Close();
+            }
+            catch (SqlException ex)
+            {
+                throw new ApplicationException("Error al insertar el Alumno");
+                try
+                {
+                    // Attempt to roll back the transaction.
+                    sqltran.Rollback();
+                }
+                catch (Exception exRollback)
+                {
+                    // Throws an InvalidOperationException if the connection 
+                    // is closed or the transaction has already been rolled 
+                    // back on the server.
+                    Console.WriteLine(exRollback.Message);
+                }
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
--- SI-ASAv6.3/Transacciones/InscripcionAlumnoACursos.aspx
+++ SI-ASAv6.3/Transacciones/InscripcionAlumnoACursos.aspx
@@ -3,5 +3,9 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+<label>Lista de alumnos:</label><asp:ListBox ID="listaAlumnos" runat="server"></asp:ListBox><br />
+<label>Curso:</label><asp:DropDownList ID="dropCurso" runat="server">
+    </asp:DropDownList><br />
+    <asp:Button ID="btnRegistrar" runat="server" Text="Registrar" />
 </asp:Content>
 
